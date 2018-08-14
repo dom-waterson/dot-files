4a5da14bcab10b1a78c54c856986257ee74b1258
Dom Waterson <dom_waterson@hotmail.com>
2018-08-14 22:16:34 +0100

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,12 @@
+{
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "editor.formatOnPaste": true,
+    "editor.formatOnType": false,
+    "eslint.autoFixOnSave": true,
+    "eslint.alwaysShowStatus": true,
+    "editor.formatOnSave": true,
+    // turn it off for JS
+    "[javascript]": {
+        "editor.formatOnSave": false
+    }
+}
